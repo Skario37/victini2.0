ab7ac66ab97402e097286eb5f2e545c2aac5e753
Kevin <kevin.courriels@gmail.com>
2021-07-13 19:32:50 +0200

    Add intents and partials and remove unused events or in future

--- src/index.js
+++ src/index.js
@@ -9,7 +9,16 @@ dotenv.config();
  */
 (() => {
   // Configure VICTINI as the whole application
-  const client = new Client();
+  const client = new Client({
+    partials: ["USER", "MESSAGE", /*"CHANNEL",*/ "REACTION"],
+    ws: { 
+      intents: [
+        "GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", 
+        "GUILD_MESSAGE_REACTIONS", "GUILD_MESSAGE_TYPING", "DIRECT_MESSAGES", "DIRECT_MESSAGE_REACTIONS", 
+        "DIRECT_MESSAGE_TYPING"
+      ] 
+    }
+  });
   client.env = process.env.NODE_ENV;
 
   // Create Discord collections
